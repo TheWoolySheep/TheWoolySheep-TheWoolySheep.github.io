e72b1a3abd984ed26f8e60d0f646e290a1245725
Mari <142839419+TheWoolySheep@users.noreply.github.com>
2024-02-09 09:10:25 -0700

    Add files via upload

--- firstjava.js
+++ firstjava.js
@@ -2,5 +2,7 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "I am Mari and taking Web Design 2";
 }
 
-
+function myfunction() {
+document.body.style.backgroundColor = "blue";
+}
 
